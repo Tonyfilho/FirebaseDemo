8caf74c652b9b4fc892c8debdef003eba9c54018
tony filho <antonio.m.filho>
2021-02-02 16:58:28 +0000

    3º  stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirebaseDatabase } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  courses: any[]= [];
+  constructor(private db: AngularFireDatabase){
+    this.courses.push( this.db.list<any>('/courses').query.get());
+    console.log(this.courses, "nosso courses");
+
+  }
   title = 'firebase-demo';
-}
+
+
+
+
+}// end class
