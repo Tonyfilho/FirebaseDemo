3418ef7817e4dce0dfb39e02d247ad13842bf48f
tony filho <antonio.m.filho>
2021-02-03 17:01:43 +0000

    2º stage subscribes, async , list and object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FirebaseDatabase } from 'angularfire2';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  courses: any[]= [];
-  constructor(private db: AngularFireDatabase){
-    this.courses.push( this.db.list<any>('/courses').query.get());
-    console.log(this.courses, "nosso courses");
+export class AppComponent  {
+  courses$: Observable<any>;
+  courses$04: Observable<any>;
+  courses: any[] = [];
+  subscrition:Subscription;
 
-  }
-  title = 'firebase-demo';
+  constructor(private db: AngularFireDatabase) {
+   this.subscrition = this.db
+    .list('/courses')
+    .valueChanges()
+    .subscribe((data) => {
+      (this.courses = data), console.log(data)});
+      
+    this.courses$ = this.db.list('/courses').valueChanges();
+    this.courses$04 = this.db.object('/courses/4').valueChanges();
+
+   };
 
 
-
-
-}// end class
+} // end class
