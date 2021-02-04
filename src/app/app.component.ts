d1b25fc3923ac0c4f481001b521e9825f943e9f7
tony filho <antonio.m.filho>
2021-02-04 13:46:27 +0000

    1 commit save data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { FirebaseDatabase } from 'angularfire2';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -9,8 +8,11 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent  {
-  courses$: Observable<any>;
+  courses$: Observable<any[]>;//todo retorno do metodo List é um Observable<any[]>
+  courses$New: AngularFireObject<any>;
+  courses$Add: Observable<any>;
   courses$04: Observable<any>;
+  author: Observable<any>;
   courses: any[] = [];
   subscrition:Subscription;
 
@@ -19,12 +21,24 @@ export class AppComponent  {
     .list('/courses')
     .valueChanges()
     .subscribe((data) => {
-      (this.courses = data), console.log(data)});
-      
+      (this.courses = data)});
+
     this.courses$ = this.db.list('/courses').valueChanges();
     this.courses$04 = this.db.object('/courses/4').valueChanges();
+    this.author = this.db.object('courses/author/1').valueChanges();
+    this.courses$New = this.db.object('/courses');
+    this.courses$Add = this.courses$New.valueChanges();
 
    };
+
+   add(data: HTMLInputElement){
+     console.log(data.value)
+     this.courses$New.set({value: data.value});
+     this.courses$
+
+     data.value = '';
+
+   }
 
 
 } // end class
